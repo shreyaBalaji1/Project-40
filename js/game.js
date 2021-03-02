6a3c5ede8d6d51fc067a4a8f0028511fb57d9bba
Shreya <123@gmail.com>
2021-03-02 16:29:21 -0600

    Fruit Catcher 2

--- js/game.js
+++ js/game.js
@@ -66,6 +66,7 @@ class Game{
                     
                      //text to display player score.
                      fill("white");
+                     textSize(25);
                      text("Player 1: " + allPlayers.player1.score, 50, 50);
                      text("Player 2: " + allPlayers.player2.score, 200, 50);
                  }
